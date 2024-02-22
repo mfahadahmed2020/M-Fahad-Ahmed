49bcfb9d41d0d6474afb87d75035dc015b427b73
mfahadahmed2020 <160360284+mfahadahmed2020@users.noreply.github.com>
2024-02-23 01:17:07 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-var message = "Hellow World";
-console.log(message);
+// lower case
+var personName = "Fahad";
+console.log("lowercase:", personName.toLowerCase());
+// upper case
+console.log("uppercase:", personName.toUpperCase());
+//Title case
+console.log("titlecase:", personName.replace(/\bw/g, function (c) { return c.toUpperCase(); }));
--- app.ts
+++ app.ts
@@ -1,2 +1,9 @@
-let message = "Hellow World";
-console.log(message);
+// lower case
+let personName: string ="Fahad"
+console.log("lowercase:", personName.toLowerCase());
+
+// upper case
+console.log("uppercase:", personName.toUpperCase());
+
+//Title case
+console.log("titlecase:", personName.replace(/\bw/g,c => c.toUpperCase()));
